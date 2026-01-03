75bbcc090c258be07e242bfb0eb39d45b7ecea7a
tinygren <timo.nygren@gmail.com>
2026-01-03 18:43:37 +0200

    virheiden käsittely ja tietokanta..

--- index.js
+++ index.js
@@ -12,11 +12,11 @@ if (process.env.NODE_ENV !== 'production') {
   const cors = require('cors')
   app.use(cors())
 }
-// Otetaan käyttöön JSON-muotoisen datan käsittely
-app.use(express.json())
+
 // 👇 Tämä rivi on tärkein
 app.use(express.static('dist'))
-
+// Otetaan käyttöön JSON-muotoisen datan käsittely
+app.use(express.json())
 // Luo oma Morgan-token:
 morgan.token('body', (req, res) => { 
   return JSON.stringify(req.body) 
@@ -65,16 +65,17 @@ app.delete('/api/people/:id', (request, response, next) => {
   Person.findByIdAndDelete(id)
     .then(() => {
       response.status(204).end()
+      console.log(`Person with id ${request.params.id} deleted`)
     })
     .catch(error => next(error))
 })
 
-function getRandomInt(min, max) {
-  const minCeiled = Math.ceil(min);
-  const maxFloored = Math.floor(max);
-  return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled); 
-  // The maximum is exclusive and the minimum is inclusive
-}
+// function getRandomInt(min, max) {
+//   const minCeiled = Math.ceil(min);
+//   const maxFloored = Math.floor(max);
+//   return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled); 
+//   // The maximum is exclusive and the minimum is inclusive
+// }
 
 app.post('/api/people', (request, response) => {
   const body = request.body
@@ -96,19 +97,50 @@ app.post('/api/people', (request, response) => {
 
 })
 app.put('/api/people/:id', (request, response, next) => {
-  const id = request.params.id
-  const body = request.body 
-  const person = {
-    name: body.name,
-    number: body.number,
-  }
-  Person.findByIdAndUpdate(id, person, { new: true })
-    .then(updatedPerson => {
-      response.json(updatedPerson)
-    })
-    .catch(error => next(error))
+  const { name, number } = request.body
+  Person.findById(request.params.id)
+      .then(person => {
+        if (!person) {
+          return response.status(404).end()
+        }
+        person.name = name       //päivitetään muistiinpanon tiedot
+        person.number = number
+
+        return person.save().then((updatedPerson) => {   //tallennetaan päivitetty muistiinpano tietokantaan
+          response.json(updatedPerson)
+        })
+      })
+      .catch(error => next(error))
 })
-  
+// app.put('/api/people/:id', (request, response, next) => {
+//   const id = request.params.id
+//   const body = request.body 
+//   const person = {
+//     name: body.name,
+//     number: body.number,
+//   }
+//   Person.findByIdAndUpdate(id, person, { new: true })
+//     .then(updatedPerson => {
+//       response.json(updatedPerson)
+//     })
+//     .catch(error => next(error))
+// })
+
+// 3️⃣ VIIMEISENÄ frontend fallback (regex!)
+app.get(/.*/, (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'), err => {
+    if (err) {
+      res.status(500).send(err)
+    }
+  })
+})
+// Error handling middleware
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+// olemattomien osoitteiden käsittely
+app.use(unknownEndpoint)
+
 // Virheidenkäsittelymiddleware
 app.use((error, request, response, next) => {
   console.error(error.message)
@@ -117,11 +149,6 @@ app.use((error, request, response, next) => {
     return response.status(400).json({ error: 'malformatted id' })
   }
   next(error)
-})
-
-// 3️⃣ VIIMEISENÄ frontend fallback (regex!)
-app.get(/.*/, (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'))
 })
 
 const PORT = process.env.PORT || 3001
